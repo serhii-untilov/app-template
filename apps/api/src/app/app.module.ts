6ee7ce9fe9bf625a3960ce863d982f02749c0b89
Serhii Untilov <s.untilov@gmail.com>
2024-10-16 17:22:45 +0300

    feat: set up swagger

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig, authConfig, googleConfig } from '../config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['.env.local', '.env', '.env.development', '.env.production'],
+            ignoreEnvVars: true,
+            // ignoreEnvFile: true,
+            load: [appConfig, authConfig, googleConfig],
+        }),
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '../../..', 'web', 'dist'),
             exclude: ['/api/(.*)'],
