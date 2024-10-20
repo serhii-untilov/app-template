d6b980ded872339f51b7e824395254b09eb851dc
Serhii Untilov <s.untilov@gmail.com>
2024-10-20 03:00:23 +0300

    test: fix user roles service spec

--- apps/api/src/resources/user-roles/user-roles.service.spec.ts
+++ apps/api/src/resources/user-roles/user-roles.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserRolesService } from './user-roles.service';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { PrismaClientExtended } from '@repo/prisma-client';
-import { PrismaService } from 'nestjs-prisma';
+import { PrismaService } from '../../prisma/prisma.service';
 
 describe('UserRolesService', () => {
     let service: UserRolesService;
